9de18583a0a626e4a3c9cba09d7a2afaea3fd0f6
Corpus77 <corpusne@gmail.com>
2023-07-11 21:13:12 +0300

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,22 @@ export class AppComponent implements OnInit {
   title = 'subject';
   comp1: string = 'default';
   comp2: string = 'default';
+  arrValue: any = [];
   
-  constructor(private readonly busService: BusService){}
+  constructor( public busService: BusService){}
   ngOnInit(): void {
     this.busService.text$.subscribe((value) => {
       this.comp1 = value;
+      this.arrValue.push(value);
+      // console.log(this.busService.text$.asObservable());
     })
     this.busService.text2$.subscribe(
       (value) => {
         this.comp2 = value;
+        this.arrValue.push(value);
       } 
     )
+    
+    
   }
 }
