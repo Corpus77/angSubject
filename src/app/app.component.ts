87ba06d76122fc047a5692e547a4679a40c22d72
Corpus77 <corpusne@gmail.com>
2023-07-14 20:32:49 +0300

    fromEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BusService } from './services/bus.service';
+import { interval, map, take, filter, debounceTime } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'subject';
   comp1: string = 'default';
   comp2: string = 'default';
   arrValue: any = [];
-  
-  constructor( public busService: BusService){}
+  coordX: number
+  constructor(public busService: BusService) { }
+
   ngOnInit(): void {
-    this.busService.text$.subscribe((value) => {
+    //
+    const el = document.querySelector('.cont');
+    const eventMouse = this.busService.eventBus(el, 'mousemove');
+    const eventMouseSuscribe = eventMouse.pipe(debounceTime(50))
+      .subscribe(evt => {
+        //console.log(evt.offsetX);
+        this.coordX = evt.clientX
+      })
+
+    //
+    console.log(eventMouseSuscribe);
+    
+    this.busService.text$.pipe(map(val => val.toUpperCase())).subscribe((value) => {
       this.comp1 = value;
       this.arrValue.push(value);
       // console.log(this.busService.text$.asObservable());
@@ -22,9 +36,18 @@ export class AppComponent implements OnInit {
       (value) => {
         this.comp2 = value;
         this.arrValue.push(value);
-      } 
+      }
     )
-    
-    
+
+
+  }
+  ngOnDestroy(): void {
+    this.busService.text$.unsubscribe()
+    this.busService.text2$.unsubscribe()
+    //this.eventMouseSuscribe.unsubscribe()
   }
 }
+
+
+
+
--- src/app/services/bus.service.ts
+++ src/app/services/bus.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { fromEvent } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class BusService {
+  //mouseMove$ = fromEvent<MouseEvent>(el, 'mousemove');
+  public eventObs$: Observable<Event>
   public text$ = new Subject<string>();
   public text2$ = new Subject<string>();
   public send(text: string) {
     this.text$.next(text);
     //console.log(this.text$);
   }
+  public eventBus(element: any, eventType: string) {
+    return this.eventObs$ = fromEvent<any>(element, eventType)
+  }
   public send2(text: string) {
     this.text2$.next(text);
     //console.log(this.text$);
   }
-  constructor() {}
+  constructor() { }
 }
